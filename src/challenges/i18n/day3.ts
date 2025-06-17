b715bf652a834b51bee3b18f1be213767cee39ea
Szymon Murawski <szymon.murawski@gmail.com>
2025-06-17 22:53:20 +0200

    feat: solve i18n day 3

---
+++ src/challenges/i18n/day3.ts
@@ -0,0 +1,67 @@
+import assert from "assert";
+import { getRlInterface } from "@shared/fileReader";
+
+async function solve(filename: string) {
+  console.log("Solving part one of file:", filename);
+
+  const rl = getRlInterface(filename);
+  let score = 0;
+
+  for await (const line of rl) {
+    if (isValidPassword(line)) {
+      score++;
+    }
+  }
+  return score;
+}
+
+function isValidPassword(password: string) {
+  if (password.length < 4 || password.length > 12) {
+    return false;
+  }
+  if (!/\d/.test(password)) {
+    return false;
+  }
+  if (!hasLowercaseChar(password)) {
+    return false;
+  }
+  if (!hasUppercaseChar(password)) {
+    return false;
+  }
+  if (!hasNonAsciiChar(password)) {
+    return false;
+  }
+  return true;
+}
+
+function hasLowercaseChar(password: string) {
+  return password.split("").some((char) => isLowercase(char));
+}
+
+function isLowercase(str: string) {
+  return str === str.toLowerCase() && str !== str.toUpperCase();
+}
+
+function hasUppercaseChar(password: string) {
+  return password.split("").some((char) => isUppercase(char));
+}
+
+function isUppercase(str: string) {
+  return str === str.toUpperCase() && str !== str.toLowerCase();
+}
+
+function hasNonAsciiChar(password: string) {
+  return password.split("").some((char) => !isAscii(char));
+}
+
+function isAscii(str: string) {
+  return str.charCodeAt(0) < 128;
+}
+
+export async function main() {
+  console.log(`Hello from day3!`);
+  const input1Result = await solve("input1.txt");
+  assert.strictEqual(input1Result, 2);
+  const input2Result = await solve("input2.txt");
+  assert.strictEqual(input2Result, 509);
+}
